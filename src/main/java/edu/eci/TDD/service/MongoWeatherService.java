a0868c4c1115270e3ef1d35113acb76bb30af10e
julitom1 <yarit.villalobos@mail.escuelaing.edu.co>
2021-09-15 10:02:12 -0500

    corrigiendo duplicado

--- src/main/java/edu/eci/TDD/service/MongoWeatherService.java
+++ src/main/java/edu/eci/TDD/service/MongoWeatherService.java
@@ -45,22 +45,5 @@ public class MongoWeatherService
         return null;
     }
 
-	@Override
-	public edu.eci.TDD.service.WeatherReport report(edu.eci.TDD.service.WeatherReportDto weatherReportDto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public edu.eci.TDD.service.WeatherReport findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<edu.eci.TDD.service.WeatherReport> findNearLocation(edu.eci.TDD.service.GeoLocation geoLocation,
-			float distanceRangeInMeters) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
